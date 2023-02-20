fe95ab06b87c7ac4a18f952574fd4fde497186aa
vinayanys <ainunissavinaya@gmail.com>
2023-02-20 15:14:23 +0700

    upload

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    "name"          : "New Addons",
+    "version"       : "11.0",
+    "author"        : "Vinaya Ainunissa",
+    "website"       : "https://www.cnnindonesia.com/",
+    "category"      : "New Module",
+    "summary"       : "Ini adalah addons baru",
+    "description"   : """
+        Latihan membuat addons baru di odoo 11
+    """,
+    "depends"       : [
+        "base",
+    ],
+    "data"          : [
+
+    ],
+    "demo"          : [],
+    "test"          : [],
+    "image"         : [],
+    "qweb"          : [],
+    "css"           : [],
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+}
